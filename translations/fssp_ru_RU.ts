1ea1fef82ea2707c9c51047769af0ee48c0a14bb
helf4ch <kirill24579@gmail.com>
2023-07-13 10:42:15 +1000

    russian translation init

--- translations/fssp_ru_RU.ts
+++ translations/fssp_ru_RU.ts
@@ -1,3 +1,418 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU"></TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>fssp::BalanceEnvelopeModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="167"/>
+        <source>Envelope frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="168"/>
+        <source>Carrier frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="169"/>
+        <source>Amplitude:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="170"/>
+        <source>Init phase:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::BaseModel</name>
+    <message>
+        <location filename="../src/basemodel.cpp" line="33"/>
+        <source>Sampling frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/basemodel.cpp" line="34"/>
+        <source>Sample&apos;s number:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/basemodel.cpp" line="35"/>
+        <source>Channel name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/basemodel.cpp" line="36"/>
+        <source>Date and time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::DelayedSingleImpulseModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="16"/>
+        <source>Delay:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::DelayedSingleJumpModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="38"/>
+        <source>Delay</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::DiscretDecreasingExpModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="60"/>
+        <source>Exponent base:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::DiscretSinModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="78"/>
+        <source>Init phase:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="79"/>
+        <source>Circular frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="80"/>
+        <source>Amplitude:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::ExpEnvelopeModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="140"/>
+        <source>Envelope width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="141"/>
+        <source>Carrier frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="142"/>
+        <source>Amplitude:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="143"/>
+        <source>Init phase:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::GraphDialog</name>
+    <message>
+        <location filename="../src/graphdialog.cpp" line="53"/>
+        <source>Graphs</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::LinearFreqModulationModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="224"/>
+        <source>Amplitude:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="225"/>
+        <source>Init phase:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="226"/>
+        <source>Start frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="227"/>
+        <source>Finish frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="29"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="29"/>
+        <source>Text files (*.txt)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="44"/>
+        <source>File type is not supported.</source>
+        <translation>Тип файла не поддерживается.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="72"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="75"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="78"/>
+        <source>About signal</source>
+        <translation>О сигнале</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="82"/>
+        <source>Model new signal...</source>
+        <translation>Моделировать новый сигнал...</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="86"/>
+        <source>Model in current signal...</source>
+        <translation>Моделировать в текущем сигнале...</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="92"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="96"/>
+        <source>&amp;Modeling</source>
+        <translation>&amp;Моделирование</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="100"/>
+        <source>&amp;Analysis</source>
+        <translation>&amp;Анализ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="102"/>
+        <source>&amp;Filter</source>
+        <translation>&amp;Фильтрация</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="104"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="106"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+</context>
+<context>
+    <name>fssp::ModelingWindow</name>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="16"/>
+        <source>Modeling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="18"/>
+        <source>Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="21"/>
+        <source>Delayed single impulse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="22"/>
+        <source>Delayed single jump</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="23"/>
+        <source>Discrete decreasing expnential</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="24"/>
+        <source>Discrete decreasing sin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="25"/>
+        <source>Rectangle grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="26"/>
+        <source>&apos;Saw&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="27"/>
+        <source>Exponential envelope model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="28"/>
+        <source>Balance envelope model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="29"/>
+        <source>Tonal envelope model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="30"/>
+        <source>Linear envelope model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="31"/>
+        <source>White noise model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="32"/>
+        <source>Normal white noise model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="33"/>
+        <source>Moving average autoregress model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="45"/>
+        <source>Calculate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="62"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="73"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modelingwindow.cpp" line="77"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::MovingAverageAutoregressModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="310"/>
+        <source>MA Coefficients:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="311"/>
+        <source>AR Coefficients:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="312"/>
+        <source>Dispersion:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::NavigationDialog</name>
+    <message>
+        <location filename="../src/navigationdialog.cpp" line="41"/>
+        <source>Signals</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::NormalWhiteNoiseModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="280"/>
+        <source>Average value:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="281"/>
+        <source>Dispersion:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::RectGridModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="101"/>
+        <source>Period:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::SawModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="122"/>
+        <source>Period:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::TonalEnvelopeModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="194"/>
+        <source>Envelope frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="195"/>
+        <source>Amplitude:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="196"/>
+        <source>Init phase:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="197"/>
+        <source>Carrier frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="198"/>
+        <source>Finish frequency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="199"/>
+        <source>Modulation depth:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>fssp::WhiteNoiseModel</name>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="255"/>
+        <source>Minimum:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/signalmodels.cpp" line="256"/>
+        <source>Maximum:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
